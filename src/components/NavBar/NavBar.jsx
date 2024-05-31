52d91b71f61f4f38d3f7a6309c0b09f78d219c58
GrzegorzPustulka <grzegorzpustulka@onet.pl>
2024-05-31 19:03:36 +0200

    routing

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,25 @@
+import { Link } from 'react-router-dom';
 import styles from './NavBar.module.css';
+import { useAuth } from '../../AuthContext';
 
-const NavBar = ({onFetchOrder}) => {
+const NavBar = ({ onFetchOrder }) => {
+  const { isAuthenticated, logout } = useAuth();
 
   return (
     <nav className={styles.navbar}>
-        <div className={styles.navLinks}>
-            <button className={styles.navItem} onClick={onFetchOrder}>Odbierz zamówienie</button>
-            <button className={styles.navItem}>Magazyn</button>
-            <button className={styles.navItem}>Moje konto</button>
-            <button className={styles.navItem}>Wyloguj</button>
-        </div>
+      <div className={styles.navLinks}>
+        <button className={styles.navItem} onClick={onFetchOrder}>Odbierz zamówienie</button>
+        <Link to="/magazyn" className={styles.navItem}>Magazyn</Link>
+        <Link to="/moje-konto" className={styles.navItem}>Moje konto</Link>
+        {isAuthenticated ? (
+          <button className={styles.navItem} onClick={logout}>Wyloguj</button>
+        ) : (
+          <>
+            <Link to="/login" className={styles.navItem}>Zaloguj</Link>
+            <Link to="/register" className={styles.navItem}>Zarejestruj</Link>
+          </>
+        )}
+      </div>
     </nav>
   );
 };
